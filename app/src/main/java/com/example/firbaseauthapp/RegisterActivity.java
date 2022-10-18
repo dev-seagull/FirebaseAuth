f1e96b70f58173974bbbdb0689d1504396672088
Kasra Alizadeh <k.alizadeh1997@gmail.com>
2022-10-18 19:15:43 +0330

    Structure of registration was added

--- app/src/main/java/com/example/firbaseauthapp/RegisterActivity.java
+++ app/src/main/java/com/example/firbaseauthapp/RegisterActivity.java
@@ -1,11 +1,18 @@
 package com.example.firbaseauthapp;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
 
 
 public class RegisterActivity extends AppCompatActivity {
@@ -40,4 +47,40 @@ public class RegisterActivity extends AppCompatActivity {
             }
         });
     }
+
+    private void RegisterUser() {
+        EditText etFirstName = findViewById(R.id.FirstName);
+        EditText etLastName = findViewById(R.id.LastName);
+        EditText etRegisterEmail = findViewById(R.id.EmailField);
+        EditText etRegisterPassword = findViewById(R.id.PasswordField);
+
+        String firstName = etFirstName.getText().toString();
+        String lastName = etLastName.getText().toString();
+        String email = etRegisterEmail.getText().toString();
+        String password = etRegisterPassword.getText().toString();
+
+        if (firstName.isEmpty() || lastName.isEmpty() || email.isEmpty() || password.isEmpty()) {
+            Toast.makeText(this, "Please fill all blank fields", Toast.LENGTH_LONG).show();
+            return;
+        }
+        UserAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            User user = new User(firstName, lastName, email);
+                            FirebaseDatabase.getInstance().getReference("users")
+                                    .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                                    .setValue(user).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<Void> task) {
+                                            showMainActivity();
+                                        }
+                                    });
+                        } else {
+                            Toast.makeText(RegisterActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
 }
---
+++ app/src/main/java/com/example/firbaseauthapp/User.java
@@ -0,0 +1,4 @@
+package com.example.firbaseauthapp;
+
+public class User {
+}
