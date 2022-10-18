ff29bc8a4e70b7baae9abf16990c4cf13bff33cd
Kasra Alizadeh <k.alizadeh1997@gmail.com>
2022-10-18 19:05:34 +0330

    Main Structure of login and registration in app were added

--- app/src/main/java/com/example/firbaseauthapp/RegisterActivity.java
+++ app/src/main/java/com/example/firbaseauthapp/RegisterActivity.java
@@ -1,14 +1,43 @@
 package com.example.firbaseauthapp;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private FirebaseAuth UserAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        UserAuth = FirebaseAuth.getInstance();
+        if(UserAuth.getCurrentUser() != null) {
+            finish();
+            return;
+        }
+
+        Button RegisterButton = findViewById(R.id.RegisterButton);
+        RegisterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                RegisterUser();
+            }
+        });
+
+        TextView textViewSwitchToLogin = findViewById(R.id.LoginLink);
+        textViewSwitchToLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                SwitchToLogin();
+            }
+        });
     }
 }
