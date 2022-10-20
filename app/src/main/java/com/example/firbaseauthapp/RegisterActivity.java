8f15c253f1d686451ce3e047f03bf02b227061a7
Kasra Alizadeh <k.alizadeh1997@gmail.com>
2022-10-20 08:59:51 +0330

    structure of our working software in login page was added

--- app/src/main/java/com/example/firbaseauthapp/RegisterActivity.java
+++ app/src/main/java/com/example/firbaseauthapp/RegisterActivity.java
@@ -1,7 +1,7 @@
 package com.example.firbaseauthapp;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -9,7 +9,6 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
