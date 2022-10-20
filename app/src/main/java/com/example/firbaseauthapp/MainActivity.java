b352ec4f7dd2732ee6beeb07dc0a87698222ccfc
Kasra Alizadeh <k.alizadeh1997@gmail.com>
2022-10-20 09:10:43 +0330

    Main program of project was added

--- app/src/main/java/com/example/firbaseauthapp/MainActivity.java
+++ app/src/main/java/com/example/firbaseauthapp/MainActivity.java
@@ -1,14 +1,87 @@
 package com.example.firbaseauthapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
-public class MainActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
+
+    private FirebaseAuth UserAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
+
+
+        UserAuth = FirebaseAuth.getInstance();
+        if (UserAuth.getCurrentUser() != null) {
+            finish();
+            return;
+        }
+
+        Button btnLogin = findViewById(R.id.LoginButton);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                authenticateUser();
+            }
+        });
+
+        TextView tvSwitchToRegister = findViewById(R.id.SwitchToRegister);
+        tvSwitchToRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                switchToRegister();
+            }
+        });
+    }
+
+    private void authenticateUser() {
+        EditText etLoginEmail = findViewById(R.id.LoginEmail);
+        EditText etLoginPassword = findViewById(R.id.LoginPassword);
+
+        String email = etLoginEmail.getText().toString();
+        String password = etLoginPassword.getText().toString();
+
+        if (email.isEmpty() || password.isEmpty()) {
+            Toast.makeText(this, "Please fill all fields", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        UserAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            showMainActivity();
+                        } else {
+                            Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    private void showMainActivity() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    private void switchToRegister() {
+        Intent intent = new Intent(this, RegisterActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
